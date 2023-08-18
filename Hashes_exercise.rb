ef8e0e2f37e29b2ca3a17dde7e05823895682418
Ebanyyy <vaniegabu@gmail.com>
2023-08-18 10:28:39 +0800

    Hashes solutions

--- Hashes_exercise.rb
+++ Hashes_exercise.rb
@@ -1,7 +1,7 @@
 # 1. Word Frequency Counter
 
 def W_frequency_count(sentence)
-  words = sentence.split(/\W+/)  #Split text into words ignoring non-word characters
+  words = sentence.split (/\W+/) #Split text into words ignoring non-word characters
   frequency = Hash.new(0)
 
   words.each do |word|
@@ -36,12 +36,11 @@ puts merge_hash(s1, s2)
 # 3. Hash Sorting
 fruits = {"Banana": 6, "Strawberry": 10, "Apple": 5}
 
-def Sort_alph(fruits)
-	fruits = fruits.sort_by { |key| }
-
+def Sort_alph(string)
+	string.sort
 end
 
-puts fruits
+puts "#{Sort_alph(fruits)}"
 
 # 4. Hash Inversion
 def hash_inversion(h)
@@ -58,3 +57,18 @@ h3 = {"Snake": 0, "Cat": 4, "Bird": 2}
 puts h1.invert
 puts h2.invert
 puts h3.invert
+
+# 5. Hash Lookup
+
+student_grades = {"bany": 90, "mia": 95, "dennis": 100}
+
+def hash_lookup(hash, key)
+	if hash.key?(key)
+		hash[key]
+	else
+		"Not Available"
+	end
+end
+
+puts hash_lookup(students, :dennis)
+puts hash_lookup(students, theo)
